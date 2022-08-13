acf7993080b43c23549593e282d9252a6d92778c
Jordan Iwase <jiwase@asu.edu>
2022-08-13 00:01:40 -1000

    Crosshairs
    Added crosshair spread to Fire, Jump and Move
    Reduce crosshair spread while aiming

--- Source/UltimateShooter/ShooterCharacter.h
+++ Source/UltimateShooter/ShooterCharacter.h
@@ -66,6 +66,11 @@ protected:
 
 	void CalculateCrosshairSpread(float DeltaTime);
 
+	void StartCrosshairBulletFire();
+
+	UFUNCTION()
+	void FinishCrosshairBulletFire();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -181,6 +186,10 @@ private:
 	// Shooting component for crosshairs spread
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crosshairs", meta = (AllowPrivateAccess = "true"))
 	float CrosshairShootingFactor;
+
+	float ShootTimeDuration;
+	bool bFiringBullet;
+	FTimerHandle CrosshairShootTimer;
 
 public:
 	// Returns CameraBoom subobject
